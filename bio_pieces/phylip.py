4d83f7bcfc156bc71357c67928bcbb2fd2a03383
Tyghe Vallard <vallardt@gmail.com>
2014-10-27 14:01:56 -0400

    adding in phylip rename stuffs

---
+++ bio_pieces/phylip.py
@@ -0,0 +1,78 @@
+# coding: utf-8
+
+from os.path import *
+import tempfile
+import subprocess
+import multiprocessing
+import pickle
+from glob import glob
+from contextlib import nested
+import os
+import shutil
+
+from Bio import SeqIO
+
+def make_renamed_phylip(inputfasta):
+    '''
+    Create a phylip formatted file from an input fasta file by
+    sorting all sequence identifiers in the fasta file and then
+    renaming each of them sequentially with Seq#_ where # is a digit
+    
+    Returns the mapping of {originalname: Seq#_ name}, phylipfile
+    '''
+    try:
+        # Get input name and extension for later
+        inputpath, extension = splitext(inputfasta)
+        # Read fasta file as a dictionary so we can easily extract all sequence ids
+        seqs = SeqIO.index(inputfasta, 'fasta')   
+        # Create temporary fasta file to store renamed sequences in
+        fd, tmpfasta = tempfile.mkstemp(suffix='.fasta')
+        # Mapping of original to -> Seq#_
+        mapping = {}
+        # Write renamed fasta sequences to temp file
+        with open(tmpfasta,'w') as fh:
+            # Write in sorted order of original name
+            for i, origname in enumerate(sorted(seqs.keys())):
+                # Original Sequence Record
+                seq = seqs[origname]
+                # New mapped sequence id
+                mappedname = 'Seq{0}_'.format(i)
+                # Print mapping
+                #print '{0} -> {1}'.format(seq.id, mappedname)
+                # Record the mapping
+                mapping[seq.id] = mappedname
+                # Rename sequence id to mapping name(>original -> >Seq#_)
+                seq.id = mappedname
+                # Write sequence to tempfile
+                fh.write(seq.format('fasta'))
+        # Convert temp fasta to sequential phylip
+        phylip_file = '{0}.phy'.format(inputpath)
+        SeqIO.convert(tmpfasta, 'fasta', phylip_file, 'phylip')
+    finally:
+        # Remove tempfile
+        os.unlink(tmpfasta)
+    return mapping, phylip_file
+
+def rename_sequences(filepath, mapping):
+    '''
+    Rename file's contents such that all occurance of mapping's
+    values are replaced with mapping's keys
+    '''
+    try:
+        # Get a tempfile to put contents of renamed in
+        fd, tmpfile = tempfile.mkstemp()
+        # Open both files
+        with nested(open(filepath),open(tmpfile,'w')) as (fhin,fhout):
+            # Iterate all lines in the inputfile
+            for line in fhin:
+                # Iterate all mappings
+                for origname, renamedname in mapping.iteritems():
+                    # Replace all occurances of Seq#_ with
+                    # the original name and write to the tempfile
+                    line = line.replace(renamedname, origname)
+                fhout.write(line)
+        # replace original file with the renamed one
+        shutil.copy(tmpfile, filepath)
+    finally:
+        # Remove tempfile
+        os.unlink(tmpfile)
---
+++ bio_pieces/tests/test_phylip.py
@@ -0,0 +1,105 @@
+from common import *
+
+class Base(BaseTester):
+    modulepath = 'bio_pieces.phylip'
+
+    def setUp(self):
+        super(Base,self).setUp()
+
+        # Number of sequences
+        self.numseqs = 10
+
+        # Orig -> Seq#_
+        self.mapping = {
+            str(i):'Seq{0}_'.format(i) for i in range(self.numseqs)
+        }
+
+    def make_fasta(self, handle, seqids, sequences):
+        ''' Write fasta sequences to handle '''
+        for id,seq in zip(seqids,sequences):
+            handle.write('>{0}\n{1}\n'.format(id,seq))
+
+    def write_mapping_fasta(self, path, mapping, keyvalues='values'):
+        # Same number of random seqs as generated sequences
+        self.randseqs = [self.rand_seq() for i in range(self.numseqs)]
+
+        # Sort the ids
+        # This fetches either the keys(orig names) or values(new names)
+        ids = sorted(getattr(self.mapping,keyvalues)())
+
+        # Write the fasta
+        with open(path,'w') as fh:
+            self.make_fasta(fh, ids, self.randseqs)
+
+        # Return the id, seq mapping
+        return zip(ids, self.randseqs)
+
+
+class TestRenameSequences(Base):
+    functionname = 'rename_sequences'
+
+    def test_replaces_contents_of_file(self):
+        inputfasta = 'input.fasta'
+        # Write fasta with all ids such as Seq#_
+        idseq = self.write_mapping_fasta(inputfasta, self.mapping)
+        # Get a mapping from that fasta
+        idseqmap = dict(idseq)
+        print idseqmap
+
+        # Should rename all seq ids to 0...9
+        r = self._C(inputfasta, self.mapping)
+
+        with open(inputfasta) as fh:
+            for line in fh: # Get id from id line
+                if line.startswith('>'):
+                    # Should be 0..9
+                    id = line.rstrip()[1:]
+                    # Gets the mapped Seq#_ name
+                    mappedid = self.mapping[id]
+                    # Get the sequence we would expect from before the rename
+                    seq = idseqmap[mappedid]
+                else: # Compare sequences
+                    eq_( seq, line.rstrip(), '{0}: {1} != {2}'.format(id,seq,line.rstrip()) )
+
+class TestMakeRenamedPhylip(Base):
+    functionname = 'make_renamed_phylip'
+
+    def test_mapping_is_correct(self):
+        inputfasta = 'input.fasta'
+
+        # Write a fasta with original names
+        idseq = self.write_mapping_fasta(inputfasta, self.mapping, 'keys')
+
+        # Run function
+        mapping, phyfile = self._C(inputfasta)
+
+        # Mapping should be 0...9 and Seq0_...Seq9_
+        for orig, new in mapping.items():
+            # Original names are simply 0..9
+            eq_( 'Seq{0}_'.format(orig), new )
+
+    def test_phylip_is_correct(self):
+        inputfasta = 'input.fasta'
+
+        # Write a fasta with original names
+        idseq = self.write_mapping_fasta(inputfasta, self.mapping, 'keys')
+        # Get a mapping from that fasta
+        idseqmap = dict(idseq)
+
+        # Run function
+        mapping, phyfile = self._C(inputfasta)
+
+        with open(phyfile) as fh:
+            lineno = 0
+            for line in fh:
+                line = line.strip()
+                if lineno == 0:
+                    numseq, seqlen = line.split()
+                    eq_( self.numseqs, int(numseq) )
+                    eq_( 10, int(seqlen) )
+                else:
+                    id, seq = line.split()
+                    # Original name is just the number portion of the name
+                    orig = int(id.replace('Seq','').replace('_',''))
+                    eq_( idseq[orig][1], seq )
+                lineno += 1
