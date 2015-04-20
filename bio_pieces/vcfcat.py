ef66edc65f77a0a0f2ba55546d70845f070e7d1e
Panciera <michael.panciera@amedwrair15370.amed.ds.army.mil>
2015-04-20 10:07:18 -0400

    Finished vcfcat documentation

--- bio_pieces/vcfcat.py
+++ bio_pieces/vcfcat.py
@@ -25,13 +25,15 @@ import pandas as pd
 import operator
 from functools import partial
 
-'''
-#TODO: Replace NaN with None ala:
-df.where(pd.notnull(df), None)
-df.fillna("NULL")
-'''
 
 def match_records(left, right, tag, threshold):
+    '''
+    :param VCFRecord left:
+    :param VCFRecord right:
+    :param str tag: the field to compare on
+    :param int threshold: the amount difference necessary to return False
+    :return bool: False if they are different according to threshold
+    '''
     l, r = flatten_vcf(left), flatten_vcf(right)
     assert tag in l and tag in r, "Tag not found in flattened record {0}".format(str(flatten_vcf))
     if not threshold:
@@ -40,6 +42,10 @@ def match_records(left, right, tag, threshold):
         return abs(l[tag] - r[tag]) < threshold
 
 def vcall(records):
+    '''
+    :param list records: list of VCFRecord objects
+    :return list: list of VCFRecords where the reference does not match the called base
+    '''
     return [rec for rec in records if rec.REF != rec.INFO['CB']]
 
 def diff(left, right, tag, threshold):
@@ -53,12 +59,14 @@ def diff(left, right, tag, threshold):
     #return [(l, r) for l, r in zip(left, right) if not match_records(l, r, tag, threshold)]
     return [l for l, r in zip(left, right) if not match_records(l, r, tag, threshold)]
 
-def validate_vcf(filename, tags):
-    #TODO: Unfortunately have to validate types here.
-    # Also Check that tags are in vcf (using flattenvcf)
-    pass
-
 def compare_value(comparator, val, rec, tag):
+    '''
+    :param builtin_function_or_method comparator: i.e. operator.eq, operator.lt
+    :param object val: the object rec[tag] will be compared against
+    :param VCFRecord rec:
+    :param str tag: the tag we're interested in the rec object, i.e. 'CBD'
+    :return bool: the result of the comparison
+    '''
     flat_vcf = flatten_vcf(rec)
     assert tag in flat_vcf, "Tag not found in flattened record {0}".format(str(flatten_vcf))
     if type(flat_vcf[tag]) != type(val):
@@ -67,6 +75,13 @@ def compare_value(comparator, val, rec, tag):
     return comparator(flat_vcf[tag], val)
 
 def not_members(vcf_list, tag, collection):
+    '''
+    Get a list comprehension of all records in vcf_list where their field(tag) is not inside collection.
+    :param list vcf_list: a list of VCFRecords
+    :param str tag: the field of interest
+    :param list collection: a list of objects
+    :return list: all records where record[tag] or record.INFO[tag] is  not in collection
+    '''
     return [rec for rec in vcf_list if flatten_vcf(rec)[tag] not in collection]
 
 ambiguous = partial(not_members, tag='CB', collection=['A', 'C', 'G', 'T'])
@@ -82,7 +97,6 @@ def filter(vcf_list, tag, opfunc, value):
     :param object value: str, int/float, or list. the value to run operator against
     :return a subset of a vcf record where the operator evaluates to true
     '''
-    #assert ( op in ['exists', 'ambiguous']  != bool(value)), "filter should not be called with operator 'exisits' OR a value."
     #ALTs are stored as lists by vcf
     if tag == 'ALT' and type(value) is str:
         value = [base.strip()  for base in value.split(',')]
@@ -91,28 +105,36 @@ def filter(vcf_list, tag, opfunc, value):
     check_value = partial(compare_value, opfunc, value)
     return [rec for rec in vcf_list if check_value(rec, tag)]
 
-
-#Can use the dtype of a column
-''' file.samples()[0]  -> returns dengue.bam etc.'''
-
 def flatten_list(A):
   return A[0] if type(A) == list else A
 
 def flatten_vcf(record):
-      _ids = ['CHROM', 'REF', 'QUAL', 'POS', 'ALT', 'FILTER', 'FORMAT', 'ID', 'INFO']
-      fields = [getattr(record, _id) for _id in _ids]
-     # converters = [str, str, str,  int, flatten_list]
-     # fields = (convert(getattr(record, _id)) for convert, _id in zip(converters, _ids))
-      d = dict( (_id, value) for  _id, value in zip(_ids, fields) )
-      d.update(dict((_id, flatten_list(field)) for _id, field in record.INFO.items()))
-      return d
+    '''
+    :param VCFRecord record: VCFRecord object
+    :return dict: all fields as a flat dictionary, including those fields in rec.INFO
+    '''
+    _ids = ['CHROM', 'REF', 'QUAL', 'POS', 'ALT', 'FILTER', 'FORMAT', 'ID', 'INFO']
+    fields = [getattr(record, _id) for _id in _ids]
+    d = dict( (_id, value) for  _id, value in zip(_ids, fields) )
+    d.update(dict((_id, flatten_list(field)) for _id, field in record.INFO.items()))
+    return d
 
 def vcf_file_to_df(vcf_records):
+    '''
+    Convert a list of vcf Records to a pandas DataFrame.
+    '''
     return pd.DataFrame(flatten_vcf(rec) for rec in vcf_records)
 
 def stat(records):
-   return vcf_file_to_df(records).describe()
+    '''
+    Get simple statistics for each field like mean, total, count, etc.
+    '''
+    return vcf_file_to_df(records).describe()
 
 def statdiff(recs_a, recs_b):
+   '''
+   Get the difference of the simple statistics using DataFrame.describe()
+   '''
+
    df1, df2 = vcf_file_to_df(recs_a), vcf_file_to_df(recs_b)
    return df1.describe() - df2.describe()
--- bio_pieces/vcfcat_main.py
+++ bio_pieces/vcfcat_main.py
@@ -10,8 +10,20 @@ Usage:
 
 Options:
     -t=<THRESH> --threshold=<THRESH>  Numerical threshold to determine difference [Default: 0]
-    -c, --count  Output the number of records.
+    -c, --count  Output the number of records instead of the actual file.
     --csv         Output simplified format
+Arguments:
+    filter:  print only vcf records matching the filter as a new vcf file.
+    exists:  only those records where the tag has a value.
+    ambiguous:  only those records where the Called Base is ambiguous.
+    vcall:  those records where the called base and reference base are different.
+    diff:   those records in <FILE1> which were different than the corresponding records (at
+    the same position) in <FILE2> according to some criteria. Providing a --tag argument will show
+    show positions where the files have different entries in that field. The threshold argument (an integer)
+    is used to determine if the values are "different enough", i.e., must be 10 more, or 10 less, etc.
+    stat:   print simple information (like mean, etc.) for all VCF fields in a tabular format.
+    statdiff:   print the (arithmetic) difference between the two file's "stat" output.
+
 '''
 
 
@@ -21,7 +33,6 @@ import operator
 from bio_pieces import vcfcat
 import sys
 import vcf
-#TODO: figure out diff output
 #TODO: Find a better way to dispatch commandline apps
 ops = ['--ge', '--le', '--gt' , '--lt' , '--eq' , '--neq']
 def validate_value(val):
@@ -39,7 +50,15 @@ def validate_vcf_file(name):
 #               Use(vcf.Reader, error='pyvcf could not read file'),
 #               Use(list))
 
-def run(raw):
+def run(raw_args):
+    '''
+    Validates the arguments by converting the VCF files into lists of VCFRecords, converting
+    threshold to int, and calling validate_value on <VALUE>.
+    returns the command string for dispatching; the new args dictionary after validating, and the first
+    filename so that that file can later be used for a vcf.VCFWriter object.
+    :param dict raw_args: the result of docopt(__doc__)
+    :return str, dict, str: the command string, the new args dict, and the first filename.
+    '''
     commands = ['vcall', 'filter', 'diff', 'stat', 'statdiff', 'exists', 'ambiguous']
     schema_dict=    {
             '<FILE1>' : Use(validate_vcf_file),
@@ -52,12 +71,18 @@ def run(raw):
              #tags.__contains__ #    error='Tag was not valid, should be one of {0}'.format(' '.join(tags)))
         }
     schema_dict.update( dict( (arg, bool) for arg in commands + ops))
-    _args = Schema(schema_dict).validate(raw)
+    _args = Schema(schema_dict).validate(raw_args)
     cmd_str = [k for (k, arg) in _args.items() if k in commands and arg][0]
-    filename = raw['<FILE1>']
+    filename = raw_args['<FILE1>']
     return cmd_str, _args, filename
 
 def dispatch_cmd(cmd_str, args):
+    '''
+    Dispatch the appropriate vcfcat.py function matching the command string (the first commandline argument)
+    :param str cmd_str: the command chosen, aka filter
+    :param dict args: validated and type-converted command-line arguments
+    :return list result: a list of VCFRecords matching the arguments
+    '''
     cmd = getattr(vcfcat, cmd_str)
     if cmd_str == 'filter':
        op = [k for k, arg in args.items() if k in ops and arg ][0]
@@ -83,6 +108,9 @@ def compute():
 
 
 def print_variant_call(rec):
+    '''
+    print vcalls in a simple tabular format
+    '''
     print("{0}\t{1}\t{2}\t{3}".format(
         rec.CHROM, rec.POS, rec.REF, rec.INFO['CB']
     ))
