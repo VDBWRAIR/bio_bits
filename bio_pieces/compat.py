1423e530a11dba73583648cb4d2a31b2dfcb5dbe
averagehat <michael.panciera.work@gmail.com>
2015-08-20 15:18:51 -0400

    adding more compatibility

--- bio_pieces/compat.py
+++ bio_pieces/compat.py
@@ -3,11 +3,24 @@ try:
 except ImportError:
     from io import StringIO
 
-from future.builtins import map
+try:
+    from BytesIO import BytesIO
+except ImportError:
+    from io import BytesIO
+
+from future.builtins import map, filter
 
 try:
-    import unittest2
+    import unittest2 as unittest
 except ImportError:
     import unittest
 
 
+try:
+    from collections import OrderedDict
+except ImportError:
+    from ordereddict import OrderedDict
+
+# Tests directory
+from os.path import dirname
+THIS = dirname(__file__)
