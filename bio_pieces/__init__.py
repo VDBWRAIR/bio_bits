41e4dbd005cc738c9a6ff10eded45d98de4acf55
Tyghe Vallard <vallardt@gmail.com>
2015-11-12 12:11:31 -0500

    deprecation warning

--- bio_pieces/__init__.py
+++ bio_pieces/__init__.py
@@ -1,2 +1,9 @@
 # Imports all of bio_bits into bio_pieces namespace
-from bio_bits import *
+import sys
+import warnings
+import bio_bits
+
+warnings.warn(
+    'bio_pieces will be removed in the future. Please use import bio_bits'
+)
+sys.modules['bio_pieces'] = bio_bits
--- tests/test_deprecation.py
+++ tests/test_deprecation.py
@@ -1,14 +1,22 @@
 from . import unittest, THIS
 import mock
+import sys
+
+dep_msg = 'bio_pieces will be removed in the future. Please use import bio_bits'
 
 @mock.patch('bio_bits.version.get_release', mock.MagicMock(return_value=1.0))
+@mock.patch.object(sys, 'stderr')
 class TestProjectRename(unittest.TestCase):
-    def test_can_import_bio_pieces(self):
+    def test_can_import_bio_pieces(self, mock_serr):
         import bio_pieces
         r = bio_pieces.version.get_release()
         self.assertEqual(1.0, r)
+        actual_msg = mock_serr.write.call_args[0][0]
+        self.assertIn(dep_msg, actual_msg)
 
-    def test_can_import_sub_module(self):
+    def test_can_import_sub_module(self, mock_serr):
         from bio_pieces import version
         r = version.get_release()
         self.assertEqual(1.0, r)
+        actual_msg = mock_serr.write.call_args[0][0]
+        self.assertIn(dep_msg, actual_msg)
