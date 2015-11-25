82831729ec046a33df8feaf656901e6d6f18e010
averagehat <michael.panciera.work@gmail.com>
2015-11-25 15:16:31 -0500

    added support for haplotype grouping

--- bio_pieces/cigar.py
+++ bio_pieces/cigar.py
@@ -1,9 +1,23 @@
 from functools import partial
-from itertools import dropwhile, takewhile, starmap
-from toolz.itertoolz import  accumulate, first, last
+from itertools import dropwhile, takewhile, starmap, combinations, imap, ifilter
+import itertools
+from toolz.itertoolz import  accumulate, first, last, drop, mapcat, take
 from toolz.functoolz import complement, compose
+from operator import itemgetter
+import fn
 import sh
 import re
+
+#from operator import xor
+#def ilen(seq): sum(1 for _ in seq)
+#indel_pos = compose(ilen, partial(takewhile, '-'.__ne__))
+#def first_indel_pos(ref, query, cigar_string):
+#    ralign, qalign = undo_cigar(ref, query, cigar_string)
+#    ins, _del = indel_pos(ralign), indel_pos(qalign)
+#    assert xor(ins, _del), "Indel contains insertion and deletion"
+#    return ins if ins else _del
+#reduce(next_cigar_position, cig_elements, (0,0))
+# read = lambda x: vim.current.buffer.append(str(x))
 
 plus_n = lambda x, n : x + n
 zero = lambda x, y : x
@@ -23,9 +37,11 @@ def transform(seqs, positions):
     elif a > b: query = fill(query, qpos)
     return  ref, query
 
-def next_cigar_position(positions, cigarval):
-    (count, sym), (refpos, qpos) = cigarval, positions
+def next_cigar_position(positions, cigarval, withtype=False):
+    if withtype: (count, sym), (refpos, qpos, oldcount, oldsym) = cigarval, positions
+    else: (count, sym), (refpos, qpos) = cigarval, positions
     f = dx_dy[sym]
+    if withtype: return f[0](refpos, count), f[1](qpos, count), count, sym
     return f[0](refpos, count), f[1](qpos, count)
 cig_diffs = partial(map, partial(next_cigar_position, (0, 0)))
 
@@ -41,16 +57,6 @@ get_cig = lambda C: map(lambda x: (int(x[0]), x[1]), split_cig(C))
 _gen_cigar = lambda c: accumulate(next_cigar_position, c, (0, 0))
 gen_cigar = compose(_gen_cigar, get_cig)
 
-#from operator import xor
-#def ilen(seq): sum(1 for _ in seq)
-#indel_pos = compose(ilen, partial(takewhile, '-'.__ne__))
-#def first_indel_pos(ref, query, cigar_string):
-#    ralign, qalign = undo_cigar(ref, query, cigar_string)
-#    ins, _del = indel_pos(ralign), indel_pos(qalign)
-#    assert xor(ins, _del), "Indel contains insertion and deletion"
-#    return ins if ins else _del
-#reduce(next_cigar_position, cig_elements, (0,0))
-# read = lambda x: vim.current.buffer.append(str(x))
 
 def get_insert_quals(ref, pos, bases):
 
@@ -61,7 +67,7 @@ def get_insert_quals(ref, pos, bases):
     has_insert = lambda l: 'I' in l[5]
     # etc.
     #samtools calcmd?
-
+# ref first
 assert undo_cigar('AAGC', 'AGTT', '1M1D1M1X1I') == ('AAGC-', 'A-GTT')
 assert list(starmap(undo_cigar, [
 ["AGG", "AG"   ,"1M1D1M"],
@@ -70,6 +76,7 @@ assert list(starmap(undo_cigar, [
 ["TA" , "TATAA", "1M3I1M"],
 ['AAGC', 'AGTT', '1M1D1M1X1I']])) ==  [('AGG', 'A-G'), ('AGG', 'A-G'), ('T-T', 'TAT'), ('T---A', 'TATAA'), ('AAGC-', 'A-GTT')]
 assert list(gen_cigar("1M2D1M")) == [(0, 0), (1, 1), (3, 1), (4, 2)]
+
 
 
 def intersection(pred, seq): return last(takewhile(complement(pred), seq))
@@ -86,9 +93,12 @@ assert reduce(next_cigar_position,  \
        takewhile(lambda x: x[1] != 'I', get_cig('1M1D1M1X1I')), (0, 0)) == (4, 3)
 
 def mutpos(cig_str, types):
+    ''':return (refpos, mutpos'''
     assert any(map(types.__contains__, cig_str)), "Cigar string %s does not have mutation of type %s" % (cig_str, types)
     return reduce(next_cigar_position, takewhile(lambda x: x[1] not in types, get_cig(cig_str)), (0, 0))
 
+foo = lambda c, t: reduce(next_cigar_position, filter(lambda x: x[1] in t, get_cig(c)), (0, 0))
+(lambda c, t: accumulate(next_cigar_position, filter(lambda x: x[1] in t, get_cig(c)), (0, 0)))
 indelpos = partial(mutpos, types='DI')
 query_indel_pos = compose(last, indelpos)
 ref_indel_pos = compose(first,  indelpos)
@@ -97,12 +107,25 @@ ref_del_pos = compose(first, partial(mutpos,  types='D'))
 ref_ins_pos = compose(first, partial(mutpos,  types='I'))
 ref_snp_pos = compose(first, partial(mutpos,  types='X'))
 
+
+def mutations(cig_str):
+    return drop(1, accumulate(partial(next_cigar_position, withtype=True), get_cig(cig_str), (0, 0, 0, 0)))
+
+snps = compose(partial(filter, lambda x: x[-1] == 'X'), mutations)
+
+def snp_rpos_base(qstring, cig_str):
+    return compose(partial(mapcat, partial(rpos_base, qstring)), snps)(cig_str)
+
+def rpos_base(qstring, (rpos, qpos, count, type)):
+    ''':return refpos, snp'''
+    return ((qstring[(qpos-count)+i], rpos +i) for i in xrange(0, count))
+
+
 #wishful thinking about complex
-{'ins' : 'I', 'del' : 'D', 'snp' 'X', 'complex' : 'X'}
+#{'ins' : 'I', 'del' : 'D', 'snp' 'X', 'complex' : 'X'}
 
 assert ref_ins_pos('1M1D1M1X1I') == 4
 #use assertRaisesRegex
-#collapse separate python scripts into individual luigi tasks(?) but tornado etc.
 try:
     mutpos('1M1D1M', 'X')
 except AssertionError:
@@ -110,3 +133,37 @@ except AssertionError:
 
 #    cigs = get_cig(CIGAR)
 #    indel_effect = sum(map(first, filter(compose('D'.__eq__, last), cigs))) - sum(map(first, filter(compose('I'.__eq__, last), cigs)))
+
+def get_info(samrow):
+    #extract query_string and cigar_string from file
+    # have to add map positions to computed rpos's.
+    qname, mpos, cigar, qstring = itemgetter(0, 3, 5, 9)(samrow.split('\t'))
+    mpos = int(mpos)
+    _snps = snp_rpos_base(qstring, cigar)
+    return map(lambda (b,i):(b, i+mpos), _snps)
+
+# split by RG, then get_info, want to keep track of qname to prob.
+#raw_longs, raw_shorts = fn.iters.partition(
+lhn = ghn = 3
+#shareCount = compose(len, set.intersection)
+shareCount = compose(len, lambda (x,y): x & y)
+is_hap = lambda n: lambda x: shareCount(x) > n
+# have to get cigar string with 'X' in it.
+if __name__ == '__main__':
+    import multiprocessing  as mp
+    import sys
+    _fn = sys.argv[1]
+    p = mp.Pool()
+    shortReads = imap(get_info, take(3, sh.samtools.view(_fn, _iter=True)))
+    shortReads = imap(set, shortReads)
+    #import ipdb; ipdb.set_trace();
+    cartProduct = itertools.product(*list(itertools.tee(shortReads)))
+    #print map(lambda (x,y): (type(x), type(y)), cartProduct)
+    shortReads = ifilter(is_hap(lhn), cartProduct)
+    for r in shortReads:
+        if r: print r
+
+
+#ghs = filter(is_hap(ghn), combinations(longReads, longReads))
+#lhs = filter(is_hap(lhn), combinations(shortReads, shortReads))
+
